7279ac544a81816837cc14115e5a3816b7df23ae
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2021-01-01 15:38:10 +0000

    updated test version of VCF to PLINK

--- ukb_wes/run_vcf_to_plink.sh
+++ ukb_wes/run_vcf_to_plink.sh
@@ -3,18 +3,18 @@
 # This script is for splitting multi-nucleotide polymorphisms,
 # then converting the VCF to PLINK format.
 #
-# Author: Nik Baya (2020-12-23)
+# Author: Nik Baya (2021-1-1)
 #
-#$ -N vcf_to_plink
-#$ -o vcf_to_plink.log
-#$ -e vcf_to_plink.errors.log
+#$ -N test_vcf_to_plink
+#$ -o test_vcf_to_plink.log
+#$ -e test_vcf_to_plink.errors.log
 #$ -q short.qe
 #$ -wd /well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/test
 #$ -l h_vmem=14G
-#$ -l h_rt=20:00:00
+#$ -l h_rt=29:00:00
 #$ -V
 #$ -P lindgren.prjc
-#$ -t 1-22
+#$ -t 20-22
 
 
 chrom=${SGE_TASK_ID}
@@ -25,16 +25,14 @@ date  && \
 echo "chr${chrom} start"  && \
 echo -e "########\n"
 
-
-vcf=ukbb-wes-oqfe-pvcf-chr${chrom}_filtered.vcf.gz
+vcf=ukb_test_data_chr${chrom}.vcf.gz
+fasta=/well/lindgren/saskia/WES/trial/ref/GRCh38_full_analysis_set_plus_decoy_hla.fa
 out=test_ukb_wes_chr${chrom}
 
 if [ ! -f ${out}.vcf.gz ]; then
 
 bcftools norm -Ou -m -any ${vcf} |
-  #bcftools norm -Ou -f human_g1k_v37.fasta |
-  bcftools annotate -Oz -x ID \
-    -I +'%CHROM:%POS:%REF:%ALT' > ${out}.vcf.gz
+  bcftools norm -Ou -f ${fasta} -o ${out}.vcf.gz
 
 echo -e "\n########"  && \
 date  && \
@@ -43,23 +41,19 @@ echo -e "########\n"
 
 fi
 
-if [ ! -f ${out}.vcf.gz.tbi ]; then
-
-tabix -p vcf ${out}.vcf.gz
+tabix -f -p vcf ${out}.vcf.gz
 
 echo -e "\n########"  && \
 date  && \
 echo "chr${chrom} tabix finished"  && \
 echo -e "########\n"
 
-fi
 
 if [ ! -f ${out}.bed ]; then
 
 plink2 --vcf ${out}.vcf.gz \
     --keep-allele-order \
-    --vcf-idspace-to _ \
-    --const-fid \
+    --double-fid \
     --allow-extra-chr  \
     --memory 13000 \
     --make-bed \
