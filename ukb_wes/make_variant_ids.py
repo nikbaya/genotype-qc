7fb531737ea924312da0c2ef65b150981372e605
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2021-01-01 17:12:41 +0000

    Create make_variant_ids.py

---
+++ ukb_wes/make_variant_ids.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#
+# Make variant IDs in the format provided by the UKB WES team
+#
+
+import pandas as pd
+import numpy as np
+import subprocess
+
+wd = '/well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/test'
+
+for chrom in range(20,23):
+  df = pd.read_csv(f'{wd}/test_ukb_wes_chr{chrom}.bim', sep='\t', names=['chr','varid','cm','pos','a1','a2'])
+
+  print(df.head())
+
+  df['a1_len'] = df.a1.str.len()
+  df['a2_len'] = df.a2.str.len()
+
+  df['varid'] = df.chr.astype(str) + ':' + df.pos.astype(str) + ':'
+  df.loc[df.a1_len==df.a2_len, 'varid'] = df.varid + df.a2 + ':' + df.a1
+  df.loc[df.a1_len>df.a2_len, 'varid'] = df.varid + 'I:'+(df.a1_len-df.a2_len).astype(str)
+  df.loc[df.a1_len<df.a2_len, 'varid'] = df.varid + 'D:'+(df.a1_len-df.a2_len).astype(str)
+
+  print(df.head())
+
+  varids = df.groupby('varid')['varid']
+  suffix = varids.cumcount()+1
+  repeats = varids.transform('size')
+
+  df['new_varid'] = np.where(repeats > 1, df['varid']+'.'+suffix.map(str).str.zfill(2), df['varid'])
+
+  df[['chr','new_varid','cm','pos','a1','a2']].to_csv(f'{wd}/test_ukb_wes_chr{chrom}.bim_new', index=False, sep='\t', header=False)
