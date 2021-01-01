a92262e4c0ba9b618643d61f0d63f1ee668e7cb7
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2021-01-01 19:22:54 +0000

    add --wd and --bfile flags

--- ukb_wes/make_variant_ids.py
+++ ukb_wes/make_variant_ids.py
@@ -9,10 +9,8 @@ import pandas as pd
 import numpy as np
 import subprocess
 
-wd = '/well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/plink'
-
-def make_variant_ids(chr):
-  df = pd.read_csv(f'{wd}/test_ukb_wes_chr{chr}.bim', sep='\t', names=['chr','varid','cm','pos','a1','a2'])
+def make_variant_ids(bfile, chr):
+  df = pd.read_csv(f'{wd}/${bfile}{chr}.bim', sep='\t', names=['chr','varid','cm','pos','a1','a2'])
 
   df['a1_len'] = df.a1.str.len()
   df['a2_len'] = df.a2.str.len()
@@ -28,19 +26,21 @@ def make_variant_ids(chr):
 
   df['new_varid'] = np.where(repeats > 1, df['varid']+'.'+suffix.map(str).str.zfill(2), df['varid'])
 
-  df[['chr','new_varid','cm','pos','a1','a2']].to_csv(f'{wd}/test_ukb_wes_chr{chr}.bim_new', index=False, sep='\t', header=False)
-
+  df[['chr','new_varid','cm','pos','a1','a2']].to_csv(f'{wd}/${bfile}{chr}.bim_new', index=False, sep='\t', header=False)
 
 def main(args):
+  wd = args.wd
   if args.chr=='all':
     for chr in range(1,23):
-      make_variant_ids(chr)
+      make_variant_ids(bfile=args.bfile, chr=chr)
   else:
-    make_variant_ids(args.chr)
+    make_variant_ids(bfile=args.bfile, chr=args.chr)
 
 if __name__=='__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--chr', help='Which chromosome to run (default: "all")', default='all')
+  parser.add_argument('--bfile', help='bfile prefix, assuming it ends with "chr"', default='ukb_wes_chr')
+  parser.add_argument('--wd', help='directory containing PLINK files', default='/well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/plink')
   args = parser.parse_args()
 
   main(args)
