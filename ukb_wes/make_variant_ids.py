c0307499e93618e81abbca9190d20aa66942fa95
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2021-01-01 18:34:00 +0000

    Update make_variant_ids.py

--- ukb_wes/make_variant_ids.py
+++ ukb_wes/make_variant_ids.py
@@ -2,17 +2,17 @@
 #
 # Make variant IDs in the format of the UKB WES team
 #
+# Author: Nikolas Baya (2020-01-01)
 
+import argparse
 import pandas as pd
 import numpy as np
 import subprocess
 
 wd = '/well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/plink'
 
-for chrom in range(1,23):
-  df = pd.read_csv(f'{wd}/test_ukb_wes_chr{chrom}.bim', sep='\t', names=['chr','varid','cm','pos','a1','a2'])
-
-  print(df.head())
+def make_variant_ids(chr):
+  df = pd.read_csv(f'{wd}/test_ukb_wes_chr{chr}.bim', sep='\t', names=['chr','varid','cm','pos','a1','a2'])
 
   df['a1_len'] = df.a1.str.len()
   df['a2_len'] = df.a2.str.len()
@@ -22,12 +22,25 @@ for chrom in range(1,23):
   df.loc[df.a1_len>df.a2_len, 'varid'] = df.varid + 'I:'+(df.a1_len-df.a2_len).abs().astype(str)
   df.loc[df.a1_len<df.a2_len, 'varid'] = df.varid + 'D:'+(df.a1_len-df.a2_len).abs().astype(str)
 
-  print(df.head())
-
   varids = df.groupby('varid')['varid']
   suffix = varids.cumcount()+1
   repeats = varids.transform('size')
 
   df['new_varid'] = np.where(repeats > 1, df['varid']+'.'+suffix.map(str).str.zfill(2), df['varid'])
 
-  df[['chr','new_varid','cm','pos','a1','a2']].to_csv(f'{wd}/test_ukb_wes_chr{chrom}.bim_new', index=False, sep='\t', header=False)
+  df[['chr','new_varid','cm','pos','a1','a2']].to_csv(f'{wd}/test_ukb_wes_chr{chr}.bim_new', index=False, sep='\t', header=False)
+
+
+def main(args):
+  if args.chr=='all':
+    for chr in range(1,23):
+      make_variant_ids(chr)
+  else:
+    make_variant_ids(args.chr)
+
+if __name__=='__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--chr', help='Which chromosome to run (default: "all")', default='all')
+  args = parser.parse_args()
+
+  main(args)
