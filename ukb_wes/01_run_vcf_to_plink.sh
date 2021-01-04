da6c3832661ac2cf641122da9309d8b083dfb5e0
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2021-01-04 15:08:03 +0000

    changed to long.qf

--- ukb_wes/01_run_vcf_to_plink.sh
+++ ukb_wes/01_run_vcf_to_plink.sh
@@ -13,14 +13,18 @@
 #$ -N vcf_to_plink
 #$ -o /well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/scripts/vcf_to_plink.log
 #$ -e /well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/scripts/vcf_to_plink.errors.log
-#$ -q short.qe
-#$ -l h_vmem=14G
-#$ -l h_rt=29:00:00
+#$ -q long.qf
+#$ -l h_rt=100:00:00
 #$ -V
+#$ -pe shmem 10
 #$ -P lindgren.prjc
-#$ -t 10,9,5,7,6,3
+#$ -t 4
 
-chrom=${SGE_TASK_ID}
+if [ ${SGE_TASK_ID} -eq 23 ]; then
+  chrom="X"
+else
+ chrom=${SGE_TASK_ID}
+fi
 
 wd=/well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink
 
@@ -34,31 +38,36 @@ tmp_vcf=${wd}/vcf/tmp-${prefix}.vcf.gz # intermediate VCF before going to PLINK
 bfile=${wd}/plink/${prefix} # PLINK bfile prefix
 
 time_check() {
-  echo -e "\n########\n$1 ($(date))\n########"
+  echo -e "\n########\n$1 (job id: ${JOB_ID}.${SGE_TASK_ID}, $(date))\n########"
 }
 
 # check that VCFs are not truncated
 vcf_check() {
-  if [ $( bcftools view $1 2>&1 | head | grep "No BGZF EOF marker" | wc -l ) -gt 0 ]; then
-    >&2 echo -e "WARNING: $1 may be truncated\nSkipping this VCF\n"
+  if [ ! -f $1 ]; then
+    >&2 time_check "ERROR: $1 does not exist. Exiting.\n"
     exit 1
+  else
+    if [ $( bcftools view $1 2>&1 | head | grep "No BGZF EOF marker" | wc -l ) -gt 0 ]; then
+      >&2 time_check "ERROR: $1 may be truncated. Exiting.\n"
+      exit 1
+    fi
   fi
 }
 
 time_check "chr${chrom} start"
 
 if [ ! -f ${tmp_vcf} ]; then
+  set -x
   vcf_check ${vcf}
-  bcftools norm -Ou -m -any ${vcf} | # split sites with multiple alleles
-    bcftools norm -Oz -f ${fasta} -o ${tmp_vcf} # left-align and normalise indels using fasta file
-
+  bcftools norm -Ou -m -any ${vcf} | \
+    bcftools norm -Oz -f ${fasta} -o ${tmp_vcf} --threads 10 # left-align and normalise indels using fasta file
+  vcf_check ${tmp_vcf}
   time_check "chr${chrom} temporary VCF finished writing"
-
+  set +x
 else
-  echo "${tmp_vcf} already exists, skipping multiallelic split and indel normalisation"
+  vcf_check ${tmp_vcf}
+  time_check "${tmp_vcf} already exists, skipping multiallelic split and indel normalisation"
 fi
-
-vcf_check ${tmp_check}
 
 # NOTE: We only check for the bed file for simplicity
 if [ ! -f ${bfile}.bed ]; then
@@ -66,18 +75,23 @@ if [ ! -f ${bfile}.bed ]; then
     --keep-allele-order \
     --double-id \
     --allow-extra-chr \
-    --memory 13500 \
+    --memory 40000 \
     --make-bed \
    --out ${bfile}
 
   time_check "chr${chrom} PLINK files finished writing"
 
 else
-  echo "${bfile}.bed already exists, skipping VCF to PLINK conversion"
+  time_check "${bfile}.bed already exists, skipping VCF to PLINK conversion"
 fi
 
 # update variant IDs
-python3 ${wd}/scripts/make_variant_ids.py --chr ${chrom} --bfile "ukb_wes_200k_chr"
+if [ -f ${bfile}.bim ]; then
+  python3 ${wd}/scripts/make_variant_ids.py --chr ${chrom} --bfile "ukb_wes_200k_chr"
+else
+  >&2 time_check "ERROR: ${bfile}.bim does not exist, new variant IDs cannot be created. Exiting.\n"
+  exit 1
+fi
 
 if [ $? -eq 0 ]; then
   if [[ ! -f ${bfile}.bim_old  && -f ${bfile}.bim_new ]]; then # add a somewhat redundant check to be sure that .bim_new exists
