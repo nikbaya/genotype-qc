d94d19bfa84da30de6d03a0fa2ed104caf6cb2b7
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2021-01-05 16:23:06 +0000

    capitalised constants

--- ukb_wes/vcf_to_plink/02_run_plink_merge.sh
+++ ukb_wes/vcf_to_plink/02_run_plink_merge.sh
@@ -7,38 +7,42 @@
 #$ -N plink_merge
 #$ -o ./plink_merge.log
 #$ -e ./plink_merge.errors.log
-#$ -q short.qf
+#$ -q long.qf
 #$ -l h_rt=60:00:00
 #$ -pe shmem 20
 #$ -V
 #$ -P lindgren.prjc
 
-wd=/well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/plink
+readonly WD=/well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/plink
 
-mergelist=${wd}/mergelist.txt
-out=${wd}/ukb_wes_200k
+readonly MERGELIST=${WD}/mergelist.txt
+readonly OUT=${WD}/ukb_wes_200k_pre_qc
 
 # create merge list file
-if [ ! -f mergelist.txt ]; then
+if [ ! -f ${MERGELIST} ]; then
   for chr in {1..24}; do
     if [ $chr -eq 23 ]; then
       chr="X"
     elif [ $chr -eq 24 ]; then
       chr="Y"
     fi
-    echo "ukb_wes_chr${chr}" >> ${mergelist}
+    echo "ukb_wes_chr${chr}" >> ${MERGELIST}
   done
 else
-  echo "Warning: ${mergelist} already exists, skipping file creation"
+  echo "Warning: ${MERGELIST} already exists, skipping file creation"
 fi
 
-if [ ! -f ${out}.bed ]; then
-  plink --merge-list ${mergelist} \
+echo -e "\nstarting plink merge (job id: ${JOB_ID}, $( date ))\n"
+cat ${MERGELIST}
+
+if [ ! -f ${OUT}.bed ]; then
+  plink --merge-list ${MERGELIST} \
     --make-bed \
     --memory 12000 \
-    --out ${out}
-  if [ -f ${out}.bed ]; then
-    echo "
+    --out ${OUT}
+  if [ -f ${OUT}.bed ]; then
+    echo "Error: ${OUT}.bed was not successfully written."
+    exit 1
 else
-  echo "Warning: ${out}.bed already exists, skipping PLINK merge"
+  echo "Warning: ${OUT}.bed already exists, skipping PLINK merge"
 fi
