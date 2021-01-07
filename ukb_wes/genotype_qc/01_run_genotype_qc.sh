11761297ec1325d2cab9b4ab31e3a67b2f3705ce
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2021-01-07 10:34:23 +0000

    updated filter thresholds, removed Fhet filter

--- ukb_wes/genotype_qc/01_run_genotype_qc.sh
+++ ukb_wes/genotype_qc/01_run_genotype_qc.sh
@@ -22,16 +22,22 @@ readonly TMP=${WD}/tmp-ukb_wes_200k
 readonly OUT=${WD}/ukb_wes_200k
 
 # filter parameters
-readonly GENO1=0.05 # include only variants with missing-rate < $GENO1 (before sample filter), important for post merge of multiple platforms
-readonly MIND=0.02     # include only samples with missing-rate < $MIND
-readonly FHET=0.2   # include only samples with -$FHET < FHET < $FHET
-readonly GENO2=0.02     # include only variants with missing-rate < $GENO2
+# NOTE: We choose less stringent filters than the default to only remove the truly bad outliers
+readonly GENO1=0.10 # include only variants with missing-rate < $GENO1 (before sample filter), important for post merge of multiple platforms (default: 0.05)
+readonly MIND=0.10     # include only samples with missing-rate < $MIND (default: 0.02)
+#readonly FHET=0.2   # include only samples with -$FHET < FHET < $FHET (default: 0.2)
+readonly GENO2=0.10     # include only variants with missing-rate < $GENO2 (default: 0.02)
 # HWE_TH=1e-10  # include only variants with HWE p-val > $HWE_TH
 # MAF=0.005     # include only variants with minor allele frequency < $MAF
 
-for f in ${BFILE}.{bed,bim,fam}; do
-  test ! -f $f && echo "Error: $f does not exist. Exiting" >&2 && exit 1
-done
+check_plink_files() {
+  if [ ! ` ls -1 $1.{bed,bim,fam} 2> /dev/null | wc -l ` -eq 3 ]; then
+    >&2 echo -e "Error: $1.{bed,bim,fam} files do not all exist. Exiting."
+    exit 1
+  fi
+}
+
+check_plink_files ${BFILE}
 
 echo -e "\n###########
 starting genotype qc (job id: ${JOB_ID}.${SGE_TASK_ID}, $(date))
@@ -42,9 +48,10 @@ OUT:   ${OUT}
 
 GENO1: ${GENO1}
 MIND:  ${MIND}
-FHET:  ${FHET}
 GENO2: ${GENO2}
 ###########"
+
+SECONDS=0
 
 ## SNP call rate 1st pass filtering
 echo "...SNP call rate 1st pass filtering ($GENO1)..."
@@ -52,6 +59,7 @@ plink --bfile ${BFILE} \
   --geno ${GENO1} \
   --make-bed \
   --out ${TMP}_geno1
+check_plink_files ${TMP}_geno
 
 ## Sample call rate filtering
 echo "...Sample call rate filtering ($MIND)..."
@@ -59,21 +67,26 @@ plink --bfile ${TMP}_geno1 \
   --mind ${MIND} \
   --make-bed \
   --out ${TMP}_mind
+check_plink_files ${TMP}_mind
 
 ## Inbreeding coefficient (Fhet)
-echo "...Sample inbreeding coefficient filtering ($FHET)..."
+echo "...Sample inbreeding coefficient calculation ($FHET)..."
 plink --bfile ${TMP}_mind \
   --het \
   --out ${TMP}
-awk -v FHET=${FHET} '{ if ($6 < -FHET || $6> FHET) print $1, $2, $6 }' ${TMP}.het > ${TMP}.remove.fhet.txt
-plink --bfile ${TMP}_mind \
-  --remove ${TMP}.remove.fhet.txt \
-  --make-bed \
-  --out ${TMP}_fhet
+#awk -v FHET=${FHET} '{ if ($6 < -FHET || $6> FHET) print $1, $2, $6 }' ${TMP}.het > ${TMP}.remove.fhet.txt
+#plink --bfile ${TMP}_mind \
+#  --remove ${TMP}.remove.fhet.txt \
+#  --make-bed \
+#  --out ${TMP}_fhet
 
 ## SNP call rate 2nd pass filtering
 echo "...SNP call rate 2nd pass filtering ($GENO2)..."
-plink --bfile ${TMP}_fhet \
+plink --bfile ${TMP}_mind \
   --geno ${GENO2} \
   --make-bed \
   --out ${OUT}
+check_plink_files ${OUT}
+
+duration=${SECONDS}
+echo "genotype qc finished, elapsed time: $( echo "scale=2; ${duration}/3600" | bc -l ) hrs (job id: ${JOB_ID}, $( date )"
