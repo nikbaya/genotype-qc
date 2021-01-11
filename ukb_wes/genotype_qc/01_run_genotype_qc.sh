3658586e954bd899f07abd1e67697c4eb10c0346
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2021-01-11 08:02:02 +0000

    move "remove withdrawn" section

--- ukb_wes/genotype_qc/01_run_genotype_qc.sh
+++ ukb_wes/genotype_qc/01_run_genotype_qc.sh
@@ -8,9 +8,12 @@
 #$ -o /well/lindgren/UKBIOBANK/nbaya/wes_200k/genotype_qc/scripts/genotype_qc.log
 #$ -e /well/lindgren/UKBIOBANK/nbaya/wes_200k/genotype_qc/scripts/genotype_qc.errors.log
 #$ -q short.qf
+##$ -l h_rt=:00:00
 #$ -pe shmem 40
 #$ -V
 #$ -P lindgren.prjc
+
+readonly OVERWRITE=0 # 0 = false, 1 = true
 
 # input
 readonly BFILE="/well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/plink/ukb_wes_200k_pre_qc"
@@ -29,18 +32,38 @@ readonly GENO2=0.10     # include only variants with missing-rate < $GENO2 (defa
 # HWE_TH=1e-10  # include only variants with HWE p-val > $HWE_TH
 # MAF=0.005     # include only variants with minor allele frequency < $MAF
 
+time_check() {
+  echo -e "\n########\n$1 (job id: ${JOB_ID}.${SGE_TASK_ID}, $(date))\n########"
+}
+
+elapsed_time() {
+  echo "elapsed time: $( echo "scale=2; $1/3600" | bc -l ) hrs"
+}
+
+plink_files_exist() {
+  [ ` ls -1 $1.{bed,bim,fam} 2> /dev/null | wc -l ` -eq 3 ]
+}
+
+raise_error() {
+  >&2 echo -e "Error: $1. Exiting."
+  exit 1
+}
+
 check_plink_files() {
-  if [ ! ` ls -1 $1.{bed,bim,fam} 2> /dev/null | wc -l ` -eq 3 ]; then
-    >&2 echo -e "Error: $1.{bed,bim,fam} files do not all exist. Exiting."
-    exit 1
+  if ! plink_files_exist $1; then
+    raise_error "$1.{bed,bim,fam} files do not all exist."
   fi
 }
 
+if [ ${OVERWRITE} -ne 0 ] && [ ${OVERWRITE} -ne 1 ]; then
+  echo -e "Error: OVERWRITE must be either 0 (do not overwrite) or 1 (overwrite). Exiting."
+  exit 1
+fi
+
 check_plink_files ${BFILE}
 
+time_check "Starting genotype qc"
 echo -e "\n###########
-Starting genotype qc (job id: ${JOB_ID}.${SGE_TASK_ID}, $(date))
-
 BFILE: ${BFILE}
 TMP:   ${TMP}
 OUT:   ${OUT}
@@ -52,25 +75,13 @@ GENO2: ${GENO2}
 
 SECONDS=0
 
-## remove withdrawn samples
-readonly WITHDRAWN="${WD}/withdrawn_samples.txt"
-if [ ! -f ${WITHDRAWN} ]; then
-  awk '{ print $1,$2 }' ${BFILE}.fam | grep "-" > ${WITHDRAWN}
-fi
-echo "Removing withdrawn samples (n=$( cat ${WITHDRAWN} | wc -l))"
-plink --bfile ${BFILE} \
-  --remove ${WITHDRAWN} \
-  --make-bed \
-  --out ${TMP}_rm_withdrawn
-check_plink_files ${TMP}_rm_withdrawn
-
 ## SNP call rate 1st pass filtering
 echo "Starting SNP call rate 1st pass filtering ($GENO1)"
-plink --bfile ${TMP}_ \
+plink --bfile ${BFILE} \
   --geno ${GENO1} \
   --make-bed \
   --out ${TMP}_geno1
-check_plink_files ${TMP}_geno
+check_plink_files ${TMP}_geno1
 
 ## Sample call rate filtering
 echo "Starting sample call rate filtering ($MIND)"
