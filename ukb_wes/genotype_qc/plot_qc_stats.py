0e76a99e7c4bcac6f70c2d4d72a3886db3ea1dba
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2021-01-14 11:38:23 +0000

    update with HWE investigation

--- ukb_wes/genotype_qc/plot_qc_stats.py
+++ ukb_wes/genotype_qc/plot_qc_stats.py
@@ -93,9 +93,9 @@ def plot_hwe_pval_qq(df, title, test, maf=None, ymax=None):
     '''
     df = df[df.TEST==test]
     if maf!=None:
-        if 'maf' not in df.columns:
-            df['maf'] = get_maf(df)
-        df = df.loc[df.maf>=maf, :]            
+        if 'MAF' not in df.columns:
+            df['MAF'] = get_maf(df)
+        df = df.loc[df.MAF>=maf, :]            
     obs = -np.log10(df.sort_values(by='P',ascending=False).P)
     exp = -np.log10(np.linspace(start=1,stop=1/df.shape[0],num=df.shape[0]))
     plt.figure(figsize=(6,4))
@@ -105,8 +105,9 @@ def plot_hwe_pval_qq(df, title, test, maf=None, ymax=None):
     plt.ylabel('Observed(-log10(p))')
     if ymax!=None:
         plt.ylim([-ymax/20, ymax])
-    plt.title(title+f'\n{f"maf≥{maf}" if maf!=None else "" }({df.shape[0]} variants)')
-    plt.savefig(f'{wd}/hwe_qq.{title.replace(" ","_")}{"."+test if test != None else ""}{f".maf_{maf}" if maf!=None else ""}{f".ymax_{ymax}" if ymax!=None else ""}.png',dpi=300)
+    plt.title(title+f'\n{f"MAF≥{maf}" if maf!=None else "" }({df.shape[0]} variants)')
+    plt.savefig(f'{wd}/hwe_qq.{get_title_str(title)}{"."+test if test != None else ""}{f".maf_{maf}" if maf!=None else ""}{f".ymax_{ymax}" if ymax!=None else ""}.png',dpi=300,
+                bbox_inches='tight')
     
 def plot_sample_het_hist(df, title, logy=False):
     df['O(HET)'] = 1-  df['O(HOM)']/df['N(NM)']
@@ -130,7 +131,7 @@ def plot_exp_obs_sample_het(df, title):
     plt.xlabel('E(HET), sample level')
     plt.ylabel('O(HET), sample level')
     # plt.ylim([df['O(HET)'].min(), df['O(HET)'].max()])
-    plt.tight_layout()
+    # plt.tight_layout()
     plt.title(title)
     plt.savefig(f'{wd}/exp_obs_sample_het.{get_title_str(title)}.png',dpi=300,
                 bbox_inches='tight')
@@ -177,10 +178,82 @@ def plot_maf_vs_lmiss(frqx, lmiss, title):
     plt.title(title)
     plt.savefig(f'{wd}/maf_vs_lmiss.{get_title_str(title)}.png',dpi=300)
     
+def plot_maf_vs_hwe(hwe, title):
+    zero = hwe[hwe.P==0]
+    nonzero = hwe[hwe.P!=0]
+
+    fig, axs = plt.subplots(2,1, figsize=(6,8))
+    plt.xlim([0,0.5])
+    axs[0].plot(nonzero.MAF, -np.log10(nonzero.P), '.', alpha=0.05, ms=0.5)
+    axs[0].set_title(r'MAF vs. -log$_{10}$(HWE p-value)')
+    axs[0].set_ylabel(r'-log$_{10}$(p)')
+    axs[0].set_xlim([0,0.5])
+    ct, bins = np.histogram(zero.MAF, np.linspace(0,0.5,21))
+    axs[1].bar((bins[:-1]+bins[1:])/2, ct*(max(-np.log10(nonzero.P))/max(ct)/2), width=((bins[:-1]+bins[1:]))[0])
+    axs[1].set_title('MAF distribution of HWE p-value=0 variants')
+    axs[1].set_ylabel('Density')
+    fig.suptitle(title, fontsize=15)
+    # plt.xscale('symlog', linthreshx=1e-3)
+    plt.xlabel('MAF')
+    
+    plt.savefig(f'{wd}/maf_vs_hwe_pval.{get_title_str(title)}.png',dpi=300,
+                bbox_inches='tight')
+    
+def plot_hwe_indels(hwe, title):
+    
+    
+def plot_ohet_ehet_vs_hwe(hwe, title, logy=False, logx=False):
+    # print(hwe[(hwe.ratio_minus_1>-1e-1)&(hwe.ratio_minus_1<-2e-2)&(hwe.P>5e-3)&(hwe.P<2e-2)].sort_values(by='ratio_minus_1').to_string(index=False))
+    # hwe = hwe[(hwe.ratio_minus_1>-1e-1)&(hwe.ratio_minus_1<-2e-2)&(hwe.P>5e-3)&(hwe.P<2e-2)].sort_values(by='ratio_minus_1')
+    
+    # hwe0['HOM(A1)'] = hwe0.GENO.str.split('/', expand=True)[0].astype(int)
+    # hwe0['HOM(A2)'] = hwe0.GENO.str.split('/', expand=True)[2].astype(int)
+    
+    # hwe = hwe0[hwe0['HOM(A1)']<=10]
+    # hwe = hwe0[hwe0['HOM(A2)']<=10]
+    if logx:
+        plt.plot(hwe['O(HET)']/hwe['E(HET)']-1, hwe.P, '.', alpha=0.04, ms=0.5)
+    else:
+        plt.plot(hwe['O(HET)']/hwe['E(HET)'], hwe.P, '.', alpha=0.5, ms=5)
+    # plt.xlim([-10**-1,-10**-2])
+    # plt.ylim([5e-3,2e-2])
+    plt.ylabel('HWE p-value')
+    if logy:
+        plt.yscale('symlog', linthreshy=1e-5)
+    if logx:
+        plt.xscale('symlog', linthreshx=1e-2)
+        plt.xlabel('O(HET)/E(HET)-1, variant level')
+    else:
+        plt.xlabel('O(HET)/E(HET), variant level')
+    plt.title(title)
+    # plt.savefig(f'{wd}/ohet_ehet_vs_hwe_pval.a1hom_le_10.{get_title_str(title)}{".logscaley" if logy else ""}{".logscaley" if logy else ""}.png',dpi=300,
+    #             bbox_inches='tight')
+
+    # hwe['ratio_minus_1'] = hwe['O(HET)']/hwe['E(HET)']-1
+    
+    plt.plot(hwe.GENO.str.split('/', expand=True)[1].astype(int), hwe['O(HET)']/hwe['E(HET)'], '.')
+    plt.xscale('log')
+    plt.xlabel('N(HET)')
+    plt.ylabel('O(HET)/E(HET)')
+    
+    plt.plot(hwe.GENO.str.split('/', expand=True)[1].astype(int), hwe.ratio_minus_1, '.')
+    plt.xscale('log')
+    plt.xlabel('N(HET)')
+    plt.ylabel('O(HET)/E(HET)-1')
+    
+    
+    
+    
+    plt.savefig(f'{wd}/ohet_ehet_vs_hwe_pval.{get_title_str(title)}{".logscaley" if logy else ""}{".logscaley" if logy else ""}.png',dpi=300,
+                bbox_inches='tight')
+
 
 def make_all_plots(bfile, title):
-    frqx = pd.read_csv(f'{wd}/{bfile}.frqx.gz', sep='\t', compression='gzip')
-    frqx['maf'] = get_maf(frqx)
+    try:
+        frqx = pd.read_csv(f'{wd}/{bfile}.frqx_v2.gz', sep='\t', compression='gzip')
+    except:
+        frqx = pd.read_csv(f'{wd}/{bfile}.frqx.gz', sep='\t', compression='gzip')
+    frqx['MAF'] = get_maf(frqx)
     # plot_a1_freq_distr(df=frqx, title=title, logy=True)
     
     het = pd.read_csv(f'{wd}/{bfile}.het', delim_whitespace=True)
@@ -195,11 +268,12 @@ def make_all_plots(bfile, title):
     plot_lmiss_hist(df=lmiss, title=title, logy=True)
     
     hwe = pd.read_csv(f'{wd}/{bfile}.hwe.gz', delim_whitespace=True, compression='gzip')
-    plot_het_distr(df=hwe, title=title, test='ALL(NP)', logy=True)
+    # plot_het_distr(df=hwe, title=title, test='ALL(NP)', logy=True)
     if hwe.shape[0]==frqx.shape[0]:
-        hwe = pd.concat((hwe, frqx[['maf']]), axis=1)
+        hwe = pd.concat((hwe, frqx[['MAF']]), axis=1)
     # hwe = hwe.merge(frqx, on=['CHR', 'SNP', 'A1', 'A2'])
-    # plot_hwe_pval_qq(df=hwe, title=title, test='ALL(NP)', maf=0.01, ymax=None)
+    plot_hwe_pval_qq(df=hwe, title=title, test='ALL(NP)', ymax=None)
+    
 
     
 
@@ -215,3 +289,10 @@ make_all_plots(bfile='ukb_wes_200k_pre_qc_wb', title='UKB WES 200k white British
 make_all_plots(bfile='ukb_wes_200k_pre_qc_wb_maf0.01', title='UKB WES 200k white British (pre QC, MAF>0.01)')
 
 make_all_plots(bfile='ukb_wes_200k_pre_qc_wb_maf0.05', title='UKB WES 200k white British (pre QC, MAF>0.05)')
+
+make_all_plots(bfile='ukb_wes_200k_pre_qc_wb_maxmaf0.01', title='UKB WES 200k white British (pre QC, MAF<0.01)')
+
+make_all_plots(bfile='test_ukb_wes_200k_clean_wb', title='UKB WES 200k white British (post QC with F-het filter)')
+
+make_all_plots(bfile='ukb_wes_200k_wb', title='UKB WES 200k white British (post QC)')
+
