0fce9131e080b3625cf37fce7e5ff2ba77b33e98
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2021-01-19 10:52:42 +0000

    test version

--- ukb_wes/vqsr/_annot_chunk.sh
+++ ukb_wes/vqsr/_annot_chunk.sh
@@ -8,17 +8,19 @@
 #$ -o /well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/test/test_annot_chunk.log
 #$ -e /well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/test/test_annot_chunk.errors.log
 #$ -q short.qf
-#$ -pe shmem 1
+#$ -pe shmem 2
 #$ -V
 #$ -P lindgren.prjc
 
 module load GATK/4.1.7.0-GCCcore-8.3.0-Java-11
 
 readonly CHUNK_IDX=${SGE_TASK_ID}
+readonly CHR=$1 # take CHR as argumen
 
 readonly WD="/well/lindgren/UKBIOBANK/nbaya/wes_200k/vcf_to_plink/test"
 readonly IN="${WD}/ukbb-wes-oqfe-pvcf-chr${CHR}.vcf.gz"
 readonly OUT="${WD}/test_scatter_annot_chr${CHR}" # output directory
+readonly OUT_CHUNK="${OUT}/ukb_wes_oqfe_pvcf_chr${CHR}.${CHUNK_IDX}of${SGE_TASK_LAST}.vcf.gz" # VCF of chunk output
 
 raise_error() {
   >&2 echo -e "Error: $1. Exiting."
@@ -43,19 +45,22 @@ vcf_check ${IN}
 
 SECONDS=0
 
-readonly INTERVAL=$( sed "${CHUNK_IDX}q;d"  ${CHR} )
+readonly INTERVALS="${OUT}/intervals_chr${CHR}.txt" # intervals file
+readonly INTERVAL=$( sed "${CHUNK_IDX}q;d"  ${INTERVALS} )
+
 echo "starting ${INTERVAL} (${CHUNK_IDX}/${SGE_TASK_LAST})"
 
-readonly MEM=4 # memory in gb used for gatk
+readonly MEM=8 # memory in gb used for gatk
 
-echo """
-gatk --java-options "-Xmx${MEM}g -Xms${MEM}g" VariantAnnotator \
-  -V ${IN} \
-  -L ${INTERVAL} \
-  -A ExcessHet -A InbreedingCoeff -A StrandOddsRatio -A QualByDepth -A FisherStrand \
-  -O ${OUT}_annot.vcf.gz
+if [ ! -f ${OUT_CHUNK} ]; then
+  gatk --java-options "-Xmx${MEM}g -Xms${MEM}g" VariantAnnotator \
+    -V ${IN} \
+    -L ${INTERVAL} \
+    -A ExcessHet -A InbreedingCoeff -A StrandOddsRatio -A QualByDepth -A FisherStrand \
+    -O ${OUT_CHUNK}
 fi
-"""
+
+vcf_check ${OUT_CHUNK}
 
 duration=${SECONDS}
-echo "chr${CHR} finished, $( elapsed_time ${duration} ) (job id: ${JOB_ID}.${SGE_TASK_ID} $( date ))"
+echo "${INTERVAL} finished, $( elapsed_time ${duration} ) (job id: ${JOB_ID}.${SGE_TASK_ID} $( date ))"
