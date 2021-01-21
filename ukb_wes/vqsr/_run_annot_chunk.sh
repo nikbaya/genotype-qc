dbd363baab6c546f0b0907fed812cc4d2dfb0f39
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2021-01-21 23:17:10 +0000

    updated for full chrom annotation

--- ukb_wes/vqsr/_run_annot_chunk.sh
+++ ukb_wes/vqsr/_run_annot_chunk.sh
@@ -14,15 +14,15 @@ module load GATK/4.1.7.0-GCCcore-8.3.0-Java-11
 
 readonly CHUNK_IDX=${SGE_TASK_ID}
 readonly CHR=${1?Error: _run_annot_chunk.sh requires the chromosome as 1st arg} # take CHR as argumen
-readonly OUT=${2?Error: _run_annot_chunk.sh requires the output directory as 2nd arg} # output directory
-readonly INTERVALS=${3?Error: _run_annot_chunk.sh requires the intervals file as 3rd arg} # intervals file
-readonly MEM=${4?Error: _run_annot_chunk.sh requires the GATK memory limit as 4th arg} # memory in gb used for gat
+readonly IN=${2?Error: _run_annot_chunk.sh requires the input VCF as 2nd arg} # input VCF to annotate
+readonly OUT_PREFIX=${3?Error: _run_annot_chunk.sh requires the output path prefix as 3rd arg} # output path prefix, e.g. /path/to/directory/ukb_wes_chr12
+readonly INTERVALS=${4?Error: _run_annot_chunk.sh requires the intervals file or range as 4th arg} # intervals file or range, e.g. chr1:43209-5899930 (if a range, the arg must being with the string "chr")
+readonly MEM=${5?Error: _run_annot_chunk.sh requires the GATK memory limit as 5th arg} # memory in gb used for gat
 
-readonly IN="/well/ukbb-wes/pvcf/oqfe/ukbb-wes-oqfe-pvcf-chr${CHR}.vcf.gz"
 # NOTE: In the following .ped file parents have different FIDs than offspring, but since GATK only looks at whether
 # the PAT/MAT fields are both nonzero when determing founder status, the founders will still be identified correctly
 readonly PED="/gpfs3/well/lindgren/UKBIOBANK/nbaya/resources/ukb12788_simplified_gatk_pedigree.ped" # pedigree file
-readonly OUT_CHUNK="${OUT}/ukb_wes_oqfe_pvcf_chr${CHR}.${CHUNK_IDX}of${SGE_TASK_LAST}.vcf.gz" # VCF of chunk output
+
 
 raise_error() {
   >&2 echo -e "Error: $1. Exiting."
@@ -41,6 +41,15 @@ elapsed_time() {
   echo "elapsed time: $( echo "scale=2; $1/3600" | bc -l ) hrs"
 }
 
+
+if [[ "$INTERVALS" == "chr"* && ! -f ${INTERVALS} ]]; then # if INTERVALS starts with "chr" and is not a file (redundant check)
+  readonly OUT="${OUT_PREFIX}.vcf.gz" # output VCF
+elif [ -f ${INTERVALS} ]; then
+  readonly OUT="${OUT_PREFIX}.${CHUNK_IDX}of${SGE_TASK_LAST}.vcf.gz" # output
+else
+  raise_error "INTERVALS variable (4th input arg) must either be 1) an intervals file with rows of the form chr1:10000393-2000003382 or 2) a string of the same form"
+fi
+
 vcf_check ${IN}
 
 SECONDS=0
@@ -48,19 +57,19 @@ SECONDS=0
 if [ -f ${INTERVALS} ]; then
   readonly INTERVAL=$( sed "${CHUNK_IDX}q;d"  ${INTERVALS} )
 else
-  raise_error "${INTERVALS} file does not exist."
+  readonly INTERVAL=${INTERVALS}
 fi
 
 echo "starting ${INTERVAL} (${CHUNK_IDX}/${SGE_TASK_LAST}) (GATK mem: ${MEM}g, job id: ${JOB_ID}.${SGE_TASK_ID} $( date ))"
 
-readonly OUT_FAILED="${OUT_CHUNK}-failed" # path for failed output VCF
+readonly OUT_FAILED="${OUT}-failed" # path for failed output VCF
 
 if [ -f ${OUT_FAILED} ]; then
   echo "Removing existing failed file: ${OUT_FAILED}"
   rm ${OUT_FAILED}
 fi
 
-if [ ! -f ${OUT_CHUNK} ]; then
+if [ ! -f ${OUT} ]; then
 
   set -x
   gatk --java-options "-Xmx${MEM}g -Xms${MEM}g -XX:-UseParallelGC" \
@@ -70,16 +79,16 @@ if [ ! -f ${OUT_CHUNK} ]; then
     -A ExcessHet -A InbreedingCoeff -A StrandOddsRatio -A QualByDepth -A FisherStrand \
     -ped ${PED} \
     --sites-only-vcf-output \
-    -O ${OUT_CHUNK}
+    -O ${OUT}
   set +x
 
-  if [ $( bcftools view -h ${OUT_CHUNK} 2>&1 | head | grep "No BGZF EOF marker" | wc -l ) -gt 0 ]; then
-    mv ${OUT_CHUNK} ${OUT_FAILED}
+  if [ $( bcftools view -h ${OUT} 2>&1 | head | grep "No BGZF EOF marker" | wc -l ) -gt 0 ]; then
+    mv ${OUT} ${OUT_FAILED}
     raise_error "GATK VariantAnnotator for chr${CHR} (${CHUNK_IDX}/${SGE_TASK_LAST}) did not successfully write output VCF, failed output VCF has been tagged with suffix \"-failed\" (job id: ${JOB_ID}.${SGE_TASK_ID} $( date ))"
   fi
 fi
 
-vcf_check ${OUT_CHUNK}
+vcf_check ${OUT}
 
 duration=${SECONDS}
 echo "${INTERVAL} finished, $( elapsed_time ${duration} ) (job id: ${JOB_ID}.${SGE_TASK_ID} $( date ))"
